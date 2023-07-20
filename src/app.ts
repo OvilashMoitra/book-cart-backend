b98ae0e0a73914b786d78a001829f1a8ce87abbc
Ovilash Moitra <96794196+OvilashMoitra@users.noreply.github.com>
2023-07-20 13:26:09 +0600

    main route added

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,10 @@ import { wishlistRouter } from './app/modules/wishlist/wishlist.route'
 app.use(express.json())
 app.use(cors())
 
+app.use("/", (req, res) => {
+    res.json({ data: "working" })
+})
+
 app.use('/api/v1/user', userRouter)
 app.use('/api/v1/book', bookRouter)
 app.use('/api/v1/wishlist', wishlistRouter)
